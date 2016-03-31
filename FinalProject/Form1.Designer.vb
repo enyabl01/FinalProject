5bcf489813e6bbde45e4e88fe3278c9cf505efb3
enyabl01 <enyabl01@students.ipfw.edu>
2016-03-31 12:43:02 -0400

    Added Things

--- FinalProject/Form1.Designer.vb
+++ FinalProject/Form1.Designer.vb
@@ -22,17 +22,133 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ItemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CatagoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DepartmentToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ItemToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CatagoryToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DepartmentToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ItemToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CatagoryToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DepartmentToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddToolStripMenuItem, Me.EditToolStripMenuItem, Me.RemoveToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(555, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AddToolStripMenuItem
+        '
+        Me.AddToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ItemToolStripMenuItem, Me.CatagoryToolStripMenuItem, Me.DepartmentToolStripMenuItem})
+        Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
+        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.AddToolStripMenuItem.Text = "Add..."
+        '
+        'ItemToolStripMenuItem
+        '
+        Me.ItemToolStripMenuItem.Name = "ItemToolStripMenuItem"
+        Me.ItemToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ItemToolStripMenuItem.Text = "Item"
+        '
+        'CatagoryToolStripMenuItem
+        '
+        Me.CatagoryToolStripMenuItem.Name = "CatagoryToolStripMenuItem"
+        Me.CatagoryToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CatagoryToolStripMenuItem.Text = "Catagory"
+        '
+        'DepartmentToolStripMenuItem
+        '
+        Me.DepartmentToolStripMenuItem.Name = "DepartmentToolStripMenuItem"
+        Me.DepartmentToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DepartmentToolStripMenuItem.Text = "Department"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ItemToolStripMenuItem1, Me.CatagoryToolStripMenuItem1, Me.DepartmentToolStripMenuItem1})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.EditToolStripMenuItem.Text = "Edit..."
+        '
+        'ItemToolStripMenuItem1
+        '
+        Me.ItemToolStripMenuItem1.Name = "ItemToolStripMenuItem1"
+        Me.ItemToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.ItemToolStripMenuItem1.Text = "Item"
+        '
+        'CatagoryToolStripMenuItem1
+        '
+        Me.CatagoryToolStripMenuItem1.Name = "CatagoryToolStripMenuItem1"
+        Me.CatagoryToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.CatagoryToolStripMenuItem1.Text = "Catagory"
+        '
+        'DepartmentToolStripMenuItem1
+        '
+        Me.DepartmentToolStripMenuItem1.Name = "DepartmentToolStripMenuItem1"
+        Me.DepartmentToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.DepartmentToolStripMenuItem1.Text = "Department"
+        '
+        'RemoveToolStripMenuItem
+        '
+        Me.RemoveToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ItemToolStripMenuItem2, Me.CatagoryToolStripMenuItem2, Me.DepartmentToolStripMenuItem2})
+        Me.RemoveToolStripMenuItem.Name = "RemoveToolStripMenuItem"
+        Me.RemoveToolStripMenuItem.Size = New System.Drawing.Size(71, 20)
+        Me.RemoveToolStripMenuItem.Text = "Remove..."
+        '
+        'ItemToolStripMenuItem2
+        '
+        Me.ItemToolStripMenuItem2.Name = "ItemToolStripMenuItem2"
+        Me.ItemToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.ItemToolStripMenuItem2.Text = "Item"
+        '
+        'CatagoryToolStripMenuItem2
+        '
+        Me.CatagoryToolStripMenuItem2.Name = "CatagoryToolStripMenuItem2"
+        Me.CatagoryToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.CatagoryToolStripMenuItem2.Text = "Catagory"
+        '
+        'DepartmentToolStripMenuItem2
+        '
+        Me.DepartmentToolStripMenuItem2.Name = "DepartmentToolStripMenuItem2"
+        Me.DepartmentToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.DepartmentToolStripMenuItem2.Text = "Department"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(389, 376)
+        Me.ClientSize = New System.Drawing.Size(555, 510)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AddToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ItemToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CatagoryToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DepartmentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ItemToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents CatagoryToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents DepartmentToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents RemoveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ItemToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents CatagoryToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents DepartmentToolStripMenuItem2 As ToolStripMenuItem
 End Class
